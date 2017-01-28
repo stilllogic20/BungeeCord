640be08ac4d16e02432711597e55d25fef46266f
azure_crystal_ <stillcrystal@outlook.jp>
2017-01-28 23:57:32 +0900

    Allowed proxy to proxy access.

--- proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
+++ proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
@@ -235,9 +235,6 @@ public class DownstreamBridge extends PacketHandler
             ByteBuf brand = Unpooled.wrappedBuffer( pluginMessage.getData() );
             String serverBrand = DefinedPacket.readString( brand );
             brand.release();
-
-            Preconditions.checkState( !serverBrand.contains( bungee.getName() ), "Cannot connect proxy to itself!" );
-
             brand = ByteBufAllocator.DEFAULT.heapBuffer();
             DefinedPacket.writeString( bungee.getName() + " (" + bungee.getVersion() + ")" + " <- " + serverBrand, brand );
             pluginMessage.setData( DefinedPacket.toArray( brand ) );
